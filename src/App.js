21ed5dfd7e814ff139785d6a92c47b873646b48c
Syed Shahzad <46902964+ShahzadSyed@users.noreply.github.com>
2020-06-18 15:05:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className={`box ${isMorning ? 'dayLight' : ''}`}>
-      <h1>BootCamp Seesio 04 - UseState with Props</h1>
+      <h1>BootCamp Seesion 04 - UseState with Props</h1>
       <h2>Day Time = {isMorning ? 'Morning' : 'Night'}</h2>
       {/* <h2>Value of counter variable is : {count}</h2> */}
 
